ae1e21d9ba7ea17a27b984e6d0fd7b27156ab615
LCTjanpol <lonodpaul18@gmail.com>
2025-09-11 22:39:43 +0800

    Fix fs import - use regular fs for synchronous methods

--- backend/pages/api/shop/apply.ts
+++ backend/pages/api/shop/apply.ts
@@ -3,7 +3,8 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import formidable from 'formidable';
-import { promises as fs } from 'fs';
+import fs from 'fs';
+import { promises as fsPromises } from 'fs';
 import path from 'path';
 import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
